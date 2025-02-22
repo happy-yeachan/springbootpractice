9885ce366ae3d78ddb1ad36cc06940d7f1b8ca5a
YEACHAN OH <dus001228@naver.com>
2025-02-22 20:18:33 +0900

    feat: Dto practice code

---
+++ src/main/java/com/example/springbootpractice/dto/ItemDto.java
@@ -0,0 +1,26 @@
+package com.example.springbootpractice.dto;
+
+public class ItemDto
+{
+    private String id;
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
+
+
