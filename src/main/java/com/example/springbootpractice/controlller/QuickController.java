e3d2ba823b7e0cb5be92fb781b069f7fe86afbe5
YEACHAN OH <dus001228@naver.com>
2025-02-22 19:37:23 +0900

    feat: @GetMapping test code

--- src/main/java/com/example/springbootpractice/controlller/QuickController.java
+++ src/main/java/com/example/springbootpractice/controlller/QuickController.java
@@ -1,12 +1,30 @@
 package com.example.springbootpractice.controlller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class QuickController {
     @GetMapping("/test")
     public String test(){
+        log.info("dummy");
         return "tessssssst";
+    }
+
+    @GetMapping("/member")
+    public String getMember(@RequestParam("empNo") String empNo,
+                            @RequestParam("year") int year){
+        log.info("empNo: {}", empNo);
+        log.info("year: {}", year);
+        return "ok";
+    }
+
+    @GetMapping("/company/{id}")
+    public String getCompany(@RequestParam("id") String id){
+        log.info("id: {}", id);
+        return "ok";
     }
 }
